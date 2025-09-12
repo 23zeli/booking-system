60e6840f3b13721cb5aa7c87e6372ab66f94d3d7
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-09-12 13:28:53 +0200

    added a test file for reservations

---
+++ src/Component/Reservations.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import  Reservations, { initializeTimes } from './Reservations';
+import { describe, it, expect } from 'vitest';
+
+
+describe('initializeTimes', () => {
+    it('returns expected value', () => {
+        const expectedTimes = [
+            '17:00',
+            '18:00',
+            '19:00',
+            '20:00',
+            '21:00',
+            '22:00',
+        ];
+        const actualTimes = initializeTimes();
+        expect(actualTimes).toEqual(expectedTimes);
+    });
+});
